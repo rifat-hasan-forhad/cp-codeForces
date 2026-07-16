33dfa4548991aa040b4a4159b3b57ad97c6e696f
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-07-16 23:32:17 +0600

    problem 268A solved

---
+++ 268A/268A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>> n;
+
+    int h[n],a[n];
+    int count=0;
+    for(int i=0;i<n;i++){
+        cin>> h[i] >> a[i];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i != j){
+                if(h[i] == a[j])
+                    count++;
+            }
+        }
+    }
+    cout<< count;
+}
