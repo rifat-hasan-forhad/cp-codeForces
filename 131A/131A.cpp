1250c1d5c969c454c7e4c19e719dbc75d216c5de
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-07-17 23:12:01 +0600

    problem 131A solved

---
+++ 131A/131A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    bool ok = true;
+
+    for (int i = 1; i < s.length(); i++) {
+        if (islower(s[i])) {
+            ok = false;
+            break;
+        }
+    }
+
+    if (ok) {
+        for (int i = 0; i < s.length(); i++) {
+            if (islower(s[i]))
+                s[i] = toupper(s[i]);
+            else
+                s[i] = tolower(s[i]);
+        }
+    }
+
+    cout << s << endl;
+
+}
