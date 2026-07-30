a135b3e7136f783fc22ef05480a9063b4a4f643e
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-07-30 22:04:08 +0600

    problem 2009A solved

---
+++ 2009A/2009A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>> t;
+
+    while(t >= 1){
+        int a,b,c;
+        cin>> a >> b;
+
+        c = b-a;
+        cout<< c << "\n";
+        t--;
+    }
+}
