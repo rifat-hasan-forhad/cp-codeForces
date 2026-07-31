039cf5eeb177bbd03d480d45a2556402a21597f8
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-07-31 21:17:48 +0600

    problem 2086A solved

---
+++ 2086A/2086A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>> t;
+
+    while(t >= 1){
+        int n;
+        cin>> n;
+
+        cout<< n*2 << "\n";
+        t--;
+    }
+}
