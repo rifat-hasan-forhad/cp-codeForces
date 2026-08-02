50d1ff40ad1d473985cda9f0259bbb56448f08bf
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-08-02 20:47:23 +0600

    problem 2093A solved

---
+++ 2093A/2093A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int k;
+    cin>> k;
+
+    if(k%2 != 0)cout<< "YES" << "\n";
+    else cout<< "NO" << "\n";
+}
+
+int main(){
+    int t;
+    cin>> t;
+
+    while(t--){
+        solve();
+    }
+}
