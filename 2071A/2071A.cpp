7d1c4907596ac088db5fe40ad44180ba8d52ee42
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-08-07 17:21:24 +0600

    problem 2071A solved

---
+++ 2071A/2071A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int k;
+    cin>> k;
+
+    if (k >= 1 && (k - 1) % 3 == 0) {
+        cout << "YES" << "\n";
+    } else {
+        cout << "NO" << "\n";
+    }
+}
+
+int main(){
+    int t;
+    cin>> t;
+
+    while(t--){
+        solve();
+    }
+}
