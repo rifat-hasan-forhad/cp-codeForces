2bf757f9454c1fa0a53a4c03f35de385c2d5e2e8
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-08-09 13:10:59 +0600

    problem 2003A solved

---
+++ 2003A/2003A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>> n;
+
+    string s;
+    cin>> s;
+
+    if(s[0] != s[n-1])cout<< "YES" << endl;
+    else cout<< "NO" << endl;
+}
+
+int main(){
+    int t ;
+    cin>> t;
+
+    while(t--){
+        solve();
+    }
+}
