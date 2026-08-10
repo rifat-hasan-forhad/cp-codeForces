2875b267e05632636f09a56aa7226d92de885a66
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-08-10 21:18:10 +0600

    problem 2106B solved

---
+++ 2106B/2106B.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,x;
+    cin>> n >> x;
+
+    for(int i=0;i<x;i++){
+        cout<< i << " ";
+    }
+    for(int i=x+1;i<n;i++){
+        cout<< i << " ";
+    }
+
+    if(x<n)cout<< x;
+    cout<< endl;
+}
+
+int main(){
+    int t;
+    cin>> t;
+
+    while(t--){
+        solve();
+    }
+}
