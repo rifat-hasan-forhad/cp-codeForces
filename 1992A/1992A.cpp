5c554039328fb9c0de5d73662feff52a56efc3e4
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-08-12 23:00:45 +0600

    problem 1992A solved

---
+++ 1992A/1992A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int a,b,c;
+    cin>> a >> b >> c;
+
+    for(int i=0;i<5;i++){
+        if(a<=b && a<=c)a++;
+        else if(b<=a && b<=c)b++;
+        else c++;
+    }
+
+    cout<< a*b*c << endl;
+}
+
+int main(){
+    int t;
+    cin>> t;
+
+    while(t--){
+        solve();
+    }
+}
+
+
+
+
