0f4b4f5992bfe7343e1ee0c5fde54ae2b40c7083
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-08-16 13:09:16 +0600

    problem 2148A solved

---
+++ 2148A/2148A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int x, n;
+    cin>> x >> n;
+
+    if(n%2 == 0)cout<< 0 << endl;
+    else cout<< x << endl;
+}
+
+int main(){
+    int t;
+    cin>> t;
+
+    while(t--){
+        solve();
+    }
+}
+
+
