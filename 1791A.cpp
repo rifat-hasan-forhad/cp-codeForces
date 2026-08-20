3c8cba300b7ce0a884c4c1fb0de46df946b61e14
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-08-20 22:12:12 +0600

    problem 1791A solved

---
+++ 1791A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string a;
+    cin>> a;
+    
+    if(a=="c" || a=="o" || a=="d" || a=="e" || a=="f" || a=="o" || a=="r" || a=="c" || a=="e" || a=="s")cout<< "YES" << endl;
+    else cout<< "NO" << endl;
+}
+
+int main() {
+	int t;
+	cin>> t;
+	while(t--){
+	    solve();
+	}
+}
