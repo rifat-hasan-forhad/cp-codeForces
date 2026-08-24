c05f1f84691df3bb9c74af50f979da645327c86c
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-08-24 12:47:26 +0600

    problem 2033A solved

---
+++ 2033A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>> n;
+    
+    cout<< (n%2 == 0 ? "Sakurako" : "Kosuke") << endl;
+}
+
+int main() {
+	int t;
+	cin>> t;
+	while(t--){
+	    solve();
+	}
+}
