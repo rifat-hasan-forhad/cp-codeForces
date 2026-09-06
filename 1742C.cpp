0c9a7b08befd17fc2e444c4555594f95805335a8
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-09-06 22:20:54 +0600

    problem 1742C solved

---
+++ 1742C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string grid[8];
+    
+    for(int i=0;i<8;i++){
+        cin>> grid[i];
+    }
+    
+    for(int i=0;i<8;i++){
+        if(grid[i] == "RRRRRRRR"){
+            cout<< "R" << "\n";
+            return;
+        }
+    }
+    
+    cout<< "B" << "\n";
+}
+
+int main() {
+	int t;
+	cin>> t;
+	while(t--){
+	    solve();
+	}
+}
