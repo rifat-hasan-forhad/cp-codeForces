5d6461bf706d0dcfe686729913ba4e919e606d1d
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-09-12 21:34:48 +0600

    problem 2263A solved

---
+++ 2263A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>> n;
+    
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>> a[i];
+    }
+    
+    int ans = 0;
+    
+    for(int i=0;i<n;i++){
+        if(a[i] == 1){
+            ans++;
+        }
+    }
+    cout<< (ans > (n - 1)/2 ? "Bessie" : "Elsie") << "\n";
+}
+
+int main() {
+	int t;
+	cin>> t;
+	while(t--){
+	    solve();
+	}
+}
