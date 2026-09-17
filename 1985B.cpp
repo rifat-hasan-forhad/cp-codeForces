7324459f18508bb7becb97f7d8afc9c4758bfefa
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-09-17 19:26:51 +0600

    problem 1985B solved

---
+++ 1985B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>> n;
+    
+    cout<< (n == 3 ? 3 : 2) << "\n";
+}
+
+int main() {
+	int t;
+	cin>> t;
+	while(t--){
+	    solve();
+	}
+}
